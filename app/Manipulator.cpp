4b8bb42ef99b72880e9f8af603e3447ce9c197f1
ameyakonk <ameyakonkar@gmail.com>
2021-10-15 20:59:40 -0400

    Created Header and Cpp files for all the classes mentioned in Task 2.2.1

--- app/Manipulator.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <bits/stdc++.h>
-#include "Manipulator.hpp"
-#include "kinematics.hpp"
-#include "Map.hpp"
-#include "ControlSystem.hpp"
-#include "RRTStar.hpp"
--- include/Manipulator.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef MANIPULATOR_H
-#define MANIPULATOR_H
-#include<bits/stdc++.h>
-#include<iostream>
-
-class Manipulator {
-  
-};
-#endif

44dff2419290e8a29e771e340e0f342b47c60873
ameyakonk <ameyakonkar@gmail.com>
2021-10-15 22:58:59 -0400

    Implemented task 2.2.9-Created and implemented set_output_angles method of Inverse_Kinematics class

--- app/Inverse_kinematics.cpp
+++ app/Inverse_kinematics.cpp
@@ -21,3 +21,7 @@ void Inverse_Kinematics::set_input_angles(std::vector<double> _input_joint_angle
 std::vector<double> Inverse_Kinematics::get_input_angles() {
  return input_joint_angles;
 }
+
+void Inverse_Kinematics::set_output_angles(std::vector<double> _output_joint_angles) {
+  output_joint_angles = _output_joint_angles;
+}

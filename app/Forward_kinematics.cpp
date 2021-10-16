22b9671c6bae9ab75c33356d8966043eb380ffd8
ameyakonk <ameyakonkar@gmail.com>
2021-10-16 14:56:14 -0400

    Implemented task 2.2.26-Created and implemented set_input_angles method of Forward_Kinematics class

--- app/Forward_kinematics.cpp
+++ app/Forward_kinematics.cpp
@@ -17,3 +17,8 @@ void set_output_angles(std::vector<double> _output_joint_angles)
 {
 	output_joint_angles = _output_joint_angles;
 }
+
+void set_input_angles(std::vector<double> _input_joint_angles)
+{
+	input_joint_angles = _input_joint_angles; 
+}

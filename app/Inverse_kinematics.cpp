b6697f8d475046aa46c443d7f2bbfbc8e8603ca0
ameyakonk <ameyakonkar@gmail.com>
2021-10-15 22:44:40 -0400

    Implemented task 2.2.5-Created and implemented set_input_coordinatesmethod of Inverse_Kinematics class

---
+++ app/Inverse_kinematics.cpp
@@ -0,0 +1,11 @@
+#include "Inverse_kinematics.hpp"
+#include <cstdlib>
+#include <cmath>
+#include "Eigen/Core"
+
+using std::cout;
+using std::endl;
+
+std::vector<double> Inverse_Kinematics:: solve_FK(std::vector<double> output_joint_coordinates) {
+ return output_joint_coordinates;
+}

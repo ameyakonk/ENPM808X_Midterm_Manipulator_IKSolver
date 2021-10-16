ff6fefb6344a43891d8db56bed89fb081afb99ed
ameyakonk <ameyakonkar@gmail.com>
2021-10-16 14:51:09 -0400

    Implemented task 2.2.24-Created and implemented set_output_coordinates method of Forward_Kinematics class

---
+++ app/Forward_kinematics.cpp
@@ -0,0 +1,14 @@
+#include "Inverse_kinematics.hpp"
+#include <cstdlib>
+#include <cmath>
+#include "Eigen/Core"
+
+using std::cout;
+using std::endl;
+
+using namespace Eigen;
+
+void set_output_coordinates(std::vector<double> _output_joint_coordinates)
+{
+	output_joint_coordinates = _output_joint_coordinates;
+}

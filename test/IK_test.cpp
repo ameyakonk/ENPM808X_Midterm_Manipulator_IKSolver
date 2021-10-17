eb1474fd976d7d4a8d8abc071cb236a6f572ac14
ameyakonk <ameyakonkar@gmail.com>
2021-10-16 22:39:46 -0400

    Implemented task 2.3.3- implemented test for get_output_angles method

--- test/IK_test.cpp
+++ test/IK_test.cpp
@@ -21,3 +21,11 @@ TEST(get_input_angles_IK, should_return_set_values) {
   for(i=0 ; i<2; i++)ASSERT_EQ(_input_angles[i], I.get_input_angles()[i]);
 }
 
+TEST(get_output_angles_IK, should_return_set_values) {
+  
+  Inverse_Kinematics I;
+  std::vector <double>::size_type i=0;
+  I.set_output_angles({PI/2, PI/4, PI/6, PI/2, PI/4, PI/6});
+  std::vector <double> _output_angles({PI/2, PI/4, PI/6, PI/2, PI/4, PI/6});
+  for(i=0 ; i<6; i++)ASSERT_EQ(_output_angles[i], I.get_output_angles()[i]);
+}

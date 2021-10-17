f9cbb43e8c2785499581206e954f594fbb1e3cef
ameyakonk <ameyakonkar@gmail.com>
2021-10-16 23:20:12 -0400

    Implemented task 2.3.7- implemented test for get_dh_alpha method of Inverse Kinematics class

--- test/IK_test.cpp
+++ test/IK_test.cpp
@@ -56,3 +56,12 @@ TEST(get_dh_d_IK, should_return_set_values) {
   std::vector <double> _dh_d({0, 5, 10, 0, 0, 0});
   for(i=0 ; i<6; i++)ASSERT_EQ(_dh_d[i], I.get_dh_d()[i]);
 }
+
+TEST(get_dh_alpha_IK, should_return_set_values) {
+  
+  Inverse_Kinematics I;
+  std::vector <double>::size_type i=0;
+  I.set_dh_alpha({PI/2, PI/4, PI/6, PI/2, PI/4, PI/6});
+  std::vector <double> _dh_alpha({PI/2, PI/4, PI/6, PI/2, PI/4, PI/6});
+  for(i=0 ; i<6; i++)ASSERT_EQ(_dh_alpha[i], I.get_dh_alpha()[i]);
+}

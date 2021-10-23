9846dca2c3eabc53c04cc7ee29235f4c35debe42
ameyakonk <78075049+ameyakonk@users.noreply.github.com>
2021-10-23 19:43:48 -0400

    Removed the plot_surface part for simulation

--- app/main.cpp
+++ app/main.cpp
@@ -66,20 +66,6 @@ int main() {
   //F.solve_FK(I.get_output_angles());
   std::vector<double>::size_type i = 0;
   std::vector<std::vector<double>> x, y, z;
-    for (double i = -5; i <= 5;  i += 0.25) {
-        std::vector<double> x_row, y_row, z_row;
-        for (double j = -5; j <= 5; j += 0.25) {
-            x_row.push_back(i);
-            y_row.push_back(j);
-            z_row.push_back(::std::sin(::std::hypot(i, j)));
-        }
-        x.push_back(x_row);
-        y.push_back(y_row);
-        z.push_back(z_row);
-    }
-
-    plt::plot_surface(x, y, z);
-    plt::show();
 
   for (i = 0; i < 6; i++) {
     std::cout << I.get_output_angles()[i] << std::endl;
